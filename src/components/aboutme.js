e313b09c1600d9b2593bacb25c959a9d9bd6bf5e
rizkiwnf <rizkiwahyunf2@gmail.com>
2023-08-03 21:22:31 +0700

    add contact

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -3,7 +3,7 @@ import "react-multi-carousel/lib/styles.css";
 // import { Container, Row, Col } from "react-bootstrap";
 import colorSharp from "../assets/images/color-sharp.png"
 import myphoto from "../assets/images/foto latar biru.jpg";
-import resume from "../assets/resume/rizki-resume.pdf"
+// import resume from "../assets/resume/rizki-resume.pdf"
 
 
 
@@ -20,8 +20,8 @@ export const AboutMe = () => {
                                 <div className="col-md-8">
                                     <p>Hello.. I am a student majoring in informatics engineering in semester 7. I am looking for opportunities as a front-end developer, or fullstack developer. I can use several programming languages such as React-JS, TailwindCSS, and Laravel. besides that I have also implemented agile methods to create a project. Have the ability for problem solving and also software development. I am a person who likes to learn new things</p>
                                     <div className="button">
-                                    <a href="" download={resume}><button className="cv-button" >Download My CV</button></a>
-                                    <a href=""><button className="contact" >Let's Talk With Me</button></a>
+                                    <a href="https://drive.google.com/file/d/1ruhS8eHvSn3yw2skx1lmvdFV7wxykoor/view?usp=sharing"><button className="cv-button" >Download My CV</button></a>
+                                    <a href="https://wa.me/6285731544494"><button className="contact" >Let's Talk With Me</button></a>
                                     </div>
                                 </div>
                                 <div className="col-md-4">
